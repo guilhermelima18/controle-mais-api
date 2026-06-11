4d0b53524b1084ec7df4c6f060b0103e22479841
guilhermelima18 <guilhermelima18@hotmail.com>
2026-06-10 23:23:23 -0300

    fix: create users route

--- src/routes/users/user-create.route.ts
+++ src/routes/users/user-create.route.ts
@@ -2,14 +2,8 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { UserCreateController } from "../../controllers/users/user-create.controller";
 
 export async function userCreateRoute(fastify: FastifyInstance) {
-  fastify.post(
-    "/",
-    {
-      onRequest: [fastify.authenticate],
-    },
-    (request: FastifyRequest, reply: FastifyReply) => {
-      const usersController = new UserCreateController();
-      return usersController.handle(request, reply);
-    },
-  );
+  fastify.post("/", (request: FastifyRequest, reply: FastifyReply) => {
+    const usersController = new UserCreateController();
+    return usersController.handle(request, reply);
+  });
 }
